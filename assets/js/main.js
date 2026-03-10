a87b60b28f80e94f35c9c5ccb195d65acd79cdfc
junaidcm <junaidanvaarialahsani@gmail.com>
2026-03-10 07:32:33 +0530

    Light/Dark Theme - Food Lover Website

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,35 +48,74 @@ const swiperReviews = new Swiper(".reviews__swiper", {
 });
 
 /*=============== SHOW SCROLL UP ===============*/
-const scrollUp = () =>{
-	const scrollUp = document.getElementById('scroll-up')
-   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 350
-	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-						     : scrollUp.classList.remove('show-scroll')
-}
-window.addEventListener('scroll', scrollUp)
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // Add the .scroll-header class if the bottom scroll of the viewport is greater than 350
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
-// Link the ID of each section (section id="home") to each link (a href="#home") 
+// Link the ID of each section (section id="home") to each link (a href="#home")
 // and activate the link with the class .active-link
 const scrollActive = () => {
-   // We get the position by scrolling down
-   const scrollY = window.scrollY
+  // We get the position by scrolling down
+  const scrollY = window.scrollY;
 
-   sections.forEach(section => {
-      const id = section.id, // id of each section
-            top = section.offsetTop - 50, // Distance from the top edge
-            height = section.offsetHeight, // Element height
-            link = document.querySelector('.nav__menu a[href*=' + id + ']') // id nav link
+  sections.forEach((section) => {
+    const id = section.id, // id of each section
+      top = section.offsetTop - 50, // Distance from the top edge
+      height = section.offsetHeight, // Element height
+      link = document.querySelector(".nav__menu a[href*=" + id + "]"); // id nav link
 
-      if(!link) return
+    if (!link) return;
 
-      link.classList.toggle('active-link', scrollY > top && scrollY <= top + height)
-   })
-}
-window.addEventListener('scroll', scrollActive)
+    link.classList.toggle(
+      "active-link",
+      scrollY > top && scrollY <= top + height,
+    );
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
 /*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-fill";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-fill" : "ri-sun-fill";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme,
+  );
+  themeButton.classList[selectedIcon === "ri-moon-fill" ? "add" : "remove"](
+    iconTheme,
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
